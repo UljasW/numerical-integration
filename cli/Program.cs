6309e1c82144b193990b21fc56e47a999caf060c
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2023-09-16 12:56:51 +0200

    working

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.CompilerServices;
+using integration.service;
+
+
+IntegrationService interator = new IntegrationService();
+
+
+System.Console.WriteLine(interator.IntegrateBetween(0.05f,2,5,f));
+
+
+float f(float x){
+    float y = x;
+    return y;
+}
